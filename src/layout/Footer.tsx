3750bf4d77f862fcafae02f355ff155c34e2695f
MxAziz <hereabdulaziz@gmail.com>
2025-07-12 06:20:04 +0600

    added types interface file for IBook & IBorrowedBook

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
         <div>
           <h3 className="text-lg font-semibold mb-2">Contact Info</h3>
           <ul className="text-sm space-y-1">
-            <li>Email: support@librarymanager.com</li>
+            <li>Email: support@LibroDesk.com</li>
             <li>Phone: +880 1234-567890</li>
             <li>Address: PUST Campus, Pabna, Bangladesh</li>
           </ul>
